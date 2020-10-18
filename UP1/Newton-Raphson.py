8eac0ecdf6ac8150c09b6ed7b8125104c4f9f74f
Rubem Vasconcelos <rfsv@ic.ufal.br>
2020-10-18 14:46:03 -0300

    Update Newton-Raphson.py

--- UP1/Newton-Raphson.py
+++ UP1/Newton-Raphson.py
@@ -6,22 +6,21 @@ def Equation(x):
     y = pow(e, 0.3 * x) - 3 + (3 * x)
     return y
 
-
+# Derivada da equação dada na questão
 def DerivativeEquation(x):
     e = 2.7182818284590452353602874
     y = 0.3 * pow(e, 0.3 * x) + 3
     return y
 
 
-x0 = 0
-x1 = 0.7
+x0 = 0.7 # Estimativa inicial dada
 i = 0  # Numero de iterações
-print("i     x2                    ea\n")
-while (1):
-    i = i + 1
-    x2 = x1 - Equation(x1) / DerivativeEquation(x1)
-    ea = (x2 - x1) / x2
-    print(i, x2, ea)
-    x1 = x2
-    if (abs(ea) < 0.002):
+print("i           x1                    ea\n")
+while (1): # No loop de repetição aplica o algoritmo de Newton-Raphson
+    i = i + 1 # Aumenta a iteração
+    x1 = x0 - Equation(x0) / DerivativeEquation(x0)
+    ea = (x1 - x0) / x1 # Calculo do erro
+    print(f'{i}     {x1}      {ea}')
+    x0 = x1
+    if (abs(ea) < 0.002): # Condição de parada das iterações
         break
